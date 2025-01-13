e429cd4eb263e70cc68a8e812a766d2f97a9a17e
ScarbVis <malte@scarb.de>
2025-01-13 22:30:13 +0100

    /youtube

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+import os
+from dotenv import load_dotenv
+from commentThread import get_video_comments
 
 app = FastAPI()
 
@@ -12,3 +15,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/youtube")
+def read_comments(video_id: str = Query(...)):
+    api_key = os.getenv('GOOGLE_API_KEY')
+    if not api_key:
+        return {"error": "API key not found in environment variables."}
+    
+    comments = get_video_comments(api_key, video_id)
+    return {"comments": comments}
